890428b21f087bc3163e847803830a8e105a1b1d
PutriAradea <putriaradea777@gmail.com>
2025-03-14 22:28:16 +0700

    Acara 13

---
+++ app/Http/Controllers/Backend/PendidikanController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers\backend;
+
+use App\Models\Pendidikan;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class PendidikanController extends Controller
+{
+
+}
