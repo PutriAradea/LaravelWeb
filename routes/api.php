6af18cd4aa5632098f3b2e200c3c6cb0d909c946
PutriAradea <putriaradea777@gmail.com>
2025-03-15 13:07:57 +0700

    Acara 21 - 22

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,18 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
+use App\Http\Controllers\backend\ApiPendidikanController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
 
+
+//acara 21
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+// Rute API untuk Pendidikan
+Route::get('api_pendidikan', [ApiPendidikanController::class, 'getAll']);
+Route::get('api_pendidikan/{id}', [ApiPendidikanController::class, 'getPen']);
+Route::post('api_pendidikan', [ApiPendidikanController::class, 'createPen']);
+Route::put('api_pendidikan/{id}', [ApiPendidikanController::class, 'updatePen']);
+Route::delete('api_pendidikan/{id}', [ApiPendidikanController::class, 'deletePen']);
