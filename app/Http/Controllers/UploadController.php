991a9d8752dd6362731d9801e565cfb13e961568
PutriAradea <putriaradea777@gmail.com>
2025-03-15 10:15:49 +0700

    Acara 19 - 20

---
+++ app/Http/Controllers/UploadController.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Http\Controllers;
+ 
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\File;
+use Intervention\Image\Facades\Image; 
+use Intervention\Image\ImageManager;
+
+class UploadController extends Controller
+{
+    public function upload()
+    {
+        return view('upload');
+    }
+
+    public function proses_upload(Request $request)
+    {
+        $this->validate($request, [
+            'file' => 'required',
+            'keterangan' => 'required',
+        ]);
+
+        // Menyimpan data file yang diupload ke variabel $file
+        $file = $request->file('file');
+
+        // Nama file
+        echo 'File Name: ' . $file->getClientOriginalName() . '<br>';
+
+        // Ekstensi file
+        echo 'File Extension: ' . $file->getClientOriginalExtension() . '<br>';
+
+        // Real path
+        echo 'File Real Path: ' . $file->getRealPath() . '<br>';
+
+        // Ukuran file
+        echo 'File Size: ' . $file->getSize() . '<br>';
+
+        // Tipe MIME
+        echo 'File Mime Type: ' . $file->getMimeType();
+
+        // Tentukan folder tujuan penyimpanan
+        $tujuan_upload = 'data_file';
+
+        // Upload file ke folder tujuan
+        $file->move($tujuan_upload, $file->getClientOriginalName());
+    }
+    public function resize_upload(Request $request)
+{
+    $this->validate($request, [
+        'file' => 'required',
+        'keterangan' => 'required',
+    ]);
+
+    // TENTUKAN PATH LOKASI UPLOAD
+    $path = public_path('img/logo');
+
+    // JIKA FOLDERNYA BELUM ADA
+    if (!File::isDirectory($path)) {
+        // MAKA FOLDER TERSEBUT AKAN DIBUAT
+        File::makeDirectory($path, 0777, true);
+    }
+
+    // MENGAMBIL FILE IMAGE DARI FORM
+    $file = $request->file('file');
+
+    // MEMBUAT NAMA FILE DARI GABUNGAN TANGGAL DAN UNIQUE ID
+    $fileName = 'logo_' . uniqid() . '.' . $file->getClientOriginalExtension();
+
+    // MEMBUAT CANVAS IMAGE SEBESAR DIMENSI
+    $canvas = Image::canvas(200, 200);
+
+    // RESIZE IMAGE SESUAI DIMENSI DENGAN MEMPERTAHANKAN RATIO
+    $resizeImage = Image::make($file)->resize(null, 200, function ($constraint) {
+        $constraint->aspectRatio();
+    });
+
+    // MEMASUKAN IMAGE YANG TELAH DIRESIZE KE DALAM CANVAS
+    $canvas->insert($resizeImage, 'center');
+
+    // SIMPAN IMAGE KE FOLDER
+    if ($canvas->save($path . '/' . $fileName)) {
+        return redirect(route('upload'))->with('success', 'Data berhasil ditambahkan!');
+    } else {
+        return redirect(route('upload'))->with('error', 'Data gagal ditambahkan!');
+    }
+}
+
+}
